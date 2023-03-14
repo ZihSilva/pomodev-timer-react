04ce172ac3eaa8210bea3f19cbff5b8335633d21
zisilva <zimarlensilva@gmail.com>
2023-03-14 13:02:33 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './Button';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
+
+export function App() {
+  return (
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant='primary' />
+        <Button variant='secondary' />
+        <Button variant='success' />
+        <Button variant='danger' />
+        <Button />
+
+        <GlobalStyle/>
+      </ThemeProvider>
+  
+
+  )
+}
+
+
+
